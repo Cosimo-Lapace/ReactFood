ee575c61d7334352d14fab06b55f4ba2c091e7fb
Cosimo Lapace <120408625+Cosimo-Lapace@users.noreply.github.com>
2024-03-06 16:45:02 +0100

    fix button bug

--- cypress/pageObj/HomePage.js
+++ cypress/pageObj/HomePage.js
@@ -6,5 +6,8 @@ export default class HomePage{
     getCartButton(){
         return cy.get("#main-header > :nth-child(2) > :nth-child(2)");
     }
+    getOrderButton(){
+        return cy.get("header div:nth-child(2) button:nth-child(1)");
+    }
 
 }
--- src/utilities/button/button.jsx
+++ src/utilities/button/button.jsx
@@ -1,10 +1,17 @@
-export default function Button({ type = "button",children, onClick, $class, ...props }) {
-  if($class === "none"){
+export default function Button({
+  type = "button",
+  children,
+  onClick,
+  $class = "",
+  ...props
+}) {
+  if ($class === "none") {
     $class = "";
-  }else{
-  $class = "button " + $class;
+  } else {
+    $class = "button " + $class;
   }
 
+  console.log($class);
   return (
     <>
       <button type={type} className={$class} onClick={onClick} {...props}>
